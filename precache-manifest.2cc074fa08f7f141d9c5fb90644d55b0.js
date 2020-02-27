b063a0233fd5fc7a3ed2fcda02383e3a16c3246d
ShoppingLiao <shopping.liao@lftech.com.tw>
2020-02-27 09:35:49 +0800

    Updates

---
+++ precache-manifest.2cc074fa08f7f141d9c5fb90644d55b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd2993b4d63be274349c2256fb51601",
+    "url": "/Simple-Calculator/index.html"
+  },
+  {
+    "revision": "6c8953312b6f1c821d0b",
+    "url": "/Simple-Calculator/static/css/main.de94b6d9.chunk.css"
+  },
+  {
+    "revision": "09393a67d2c4a64142e1",
+    "url": "/Simple-Calculator/static/js/2.c6189b96.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Simple-Calculator/static/js/2.c6189b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8953312b6f1c821d0b",
+    "url": "/Simple-Calculator/static/js/main.6ecbc683.chunk.js"
+  },
+  {
+    "revision": "28175524785df3f9e0f8",
+    "url": "/Simple-Calculator/static/js/runtime-main.03e3a5c0.js"
+  }
+]);
